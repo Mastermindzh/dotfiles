ccf73b084c60e5c64aec98e1f86d8a63e53ee03f
Rick van Lieshout <info@rickvanlieshout.com>
2018-02-14 08:48:17 +0100

    removed formatting :)

--- bash/.alias.sh
+++ bash/.alias.sh
@@ -4,9 +4,9 @@ alias nodeserver='docker run --rm -p 3000:3000 -v "$PWD":/app mastermindzh/gener
 alias reactserver='docker run --rm -p 8080:8080 -v "$PWD":/app mastermindzh/generic_node'
 
 # useful docker commands
-alias stop-dockers = "docker stop $(docker ps -aq)"
-alias remove-dockers = "docker rm -v $(docker ps -aq -f 'status=exited')"
-alias clean-dockers = "docker rmi $(docker images -aq -f 'dangling=true')"
+alias stop-dockers="docker stop $(docker ps -aq)"
+alias remove-dockers="docker rm -v $(docker ps -aq -f 'status=exited')"
+alias clean-dockers="docker rmi $(docker images -aq -f 'dangling=true')"
 
 #upload text to sprunge.us
 #USAGE cat file.txt | sprunge
