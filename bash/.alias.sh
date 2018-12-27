01122b34e29ef44ee2466cdef9c7b1b9d79ec6cc
Mastermindzh <info@rickvanlieshout.com>
2018-12-27 08:27:03 +0100

    new, darker, colour scheme

--- bash/.alias.sh
+++ bash/.alias.sh
@@ -2,6 +2,8 @@
 alias phpserver='docker run --rm -p 2000:80 -v "$PWD":/var/www/html mastermindzh/php-xdebug'
 alias nodeserver='docker run --rm -p 3000:3000 -v "$PWD":/app mastermindzh/generic_node'
 alias reactserver='docker run --rm -p 8080:8080 -v "$PWD":/app mastermindzh/generic_node'
+alias mongoserver='docker run -d --rm -p 27017:27017 --name mongo-server -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=123 -v ~/.db/mongo:/data/db mongo'
+alias sqlserver='docker run -d --rm -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=YourPassword123" -p 1433:1433 -v ~/.db/mssql:/var/opt/mssql microsoft/mssql-server-linux'
 
 # useful docker commands
 alias stop-dockers='docker stop $(docker ps -aq)'
