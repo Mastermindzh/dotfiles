caec5d5d041993e7f5f4a3b1b5abc849c745b1f1
Mastermindzh <info@rickvanlieshout.com>
2019-02-20 12:51:49 +0100

    lockscreen now stops/starts spotify automatically :)

--- i3/scripts/i3lock.sh
+++ i3/scripts/i3lock.sh
@@ -3,9 +3,21 @@
 icon="$HOME/.config/i3/icons/lock.png"
 tmpbg='/tmp/screen.png'
 
+# detect whether spotify is running
+isPlaying=$(~/.config/i3/scripts/spotify-cli.sh status);
+
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -f -i "$tmpbg"
+
+# Stop music if playing
+~/.config/i3/scripts/spotify-cli.sh pause
+
+i3lock -n -f -i "$tmpbg";
+
+# if spotify was playing before we locked, resume.
+if [ $isPlaying == "Playing" ]; then
+    ~/.config/i3/scripts/spotify-cli.sh play
+fi;
