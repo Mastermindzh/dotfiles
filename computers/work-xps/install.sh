efc1f43ebf51ba5167182963eea78e2ee0e2399e
Mastermindzh <info@rickvanlieshout.com>
2019-09-16 09:16:54 +0200

    switched to different battery indicator.

--- computers/work-xps/install.sh
+++ computers/work-xps/install.sh
@@ -11,6 +11,6 @@ ln -sf "$MYPATH/Xresources.undocked" ~/.Xresources.undocked
 ln -sf "$MYPATH/docked.sh" ~/.docked.sh
 ln -sf "$MYPATH/undocked.sh" ~/.undocked.sh
 ln -sf "$MYPATH/xprofile.sh" ~/.xprofile
-    
+
 # custom (laptop specific) bashrc thingies :)    
 ln -sf "$MYPATH/custom-bashrc.sh" ~/.custom
