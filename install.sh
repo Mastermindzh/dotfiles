a83eabe03d36b93d9703c28d544e2106fa115a8c
Mastermindzh <info@rickvanlieshout.com>
2019-09-16 20:17:42 +0200

    added pulse and changed dev-pc configs

--- install.sh
+++ install.sh
@@ -98,6 +98,8 @@ function install_config {
 	ln -sf "$PWD"/config/.gitconfig ~/.gitconfig
 	ln -sf "$PWD"/config/.npmrc ~/.npmrc
 	ln -sf "$PWD"/config/user-dirs.dirs ~/.config/user-dirs.dirs
+	mkdir -p ~/.pulse
+	ln -sf "$PWD"/config/pulse/daemon.conf ~/.pulse/daemon.conf
 
 	# link system files / directories
 	sudo ln -sf "$PWD"/config/package-managers/pacman.conf /etc/pacman.conf
