7dd46f40f8f33aedd87689c2e4abc32fe2e6ea73
Mastermindzh <info@rickvanlieshout.com>
2023-01-08 14:47:04 +0100

    removed code wallpaper, switched to thunar with  engrampa, retired work-xps

--- i3/scripts/blocks/temp.sh
+++ i3/scripts/blocks/temp.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-intelTemp=$(sensors | # execute sensors
-grep -w "Core 0:" | # grep for the first core
-sed 's/([^)]*)//g' | # filter
-tr -s " " | # remove whitespace
-sed -e 's/Core 0\(.*\):/\1/' | #  get value between Core 0 and :
-cut -c 3- | rev | cut -c 7- | rev #remove clutter
+intelTemp=$(
+      sensors |                               # execute sensors
+            grep -w "Core 0:" |               # grep for the first core
+            sed 's/([^)]*)//g' |              # filter
+            tr -s " " |                       # remove whitespace
+            sed -e 's/Core 0\(.*\):/\1/' |    #  get value between Core 0 and :
+            cut -c 3- | rev | cut -c 7- | rev #remove clutter
 )
 
-amdTemp=$(sensors | grep -w "Tdie:" | sed 's/([^)]*)//g' | tr -s " " | cut -c 8- | rev | cut -c 6- | rev)
+amdTemp=$(sensors | grep -iw "tctl:" | sed 's/([^)]*)//g' | tr -s " " | cut -c 8- | rev | cut -c 2- | rev)
 
 # echo out the result
-if [ -z "$intelTemp" ]
-then
+if [ -z "$intelTemp" ]; then
       echo $amdTemp
 else
       echo $intelTemp°C
 fi
-
