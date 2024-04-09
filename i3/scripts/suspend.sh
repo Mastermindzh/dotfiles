dd5f32a6eb4551a3127afe54234dcb00d35a9083
Rick van Lieshout <info@rickvanlieshout.com>
2024-04-09 18:45:06 +0200

    fixed lock screen activation on suspend. Thanks Suayip :*

--- i3/scripts/suspend.sh
+++ i3/scripts/suspend.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # check if locked
-if !(pgrep -x "i3lock" > /dev/null)
-then
-    bash ~/.config/i3/scripts/i3lock.sh --suspend
+if ! pgrep -x "i3lock" >/dev/null; then
+  echo "not locked, locking"
+  bash ~/.config/i3/scripts/lock.sh --suspend
 fi
 
 systemctl suspend
